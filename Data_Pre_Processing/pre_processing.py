732b7ba7a47608db32780b0bbec34dc1de2cb424
Snigdha Parashar <100710845+snigdha510@users.noreply.github.com>
2022-07-14 23:29:16 +0530

    updated little bit

--- Data_Pre_Processing/pre_processing.py
+++ Data_Pre_Processing/pre_processing.py
@@ -2,19 +2,23 @@ import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
+
 def grey(img): #grascayling the image
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  
     return gray
+
 def sharpen(image): #sharpening the image
     kernel = np.array([[0, -1, 0],[-1, 5,-1],[0, -1, 0]])
     image_sharp = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
     return image_sharp
+
 def contour(gimage): # detect contour
     _, binary = cv2.threshold(gimage, 225, 255, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     image = cv2.drawContours(gimage, contours, -1, (0, 255, 0), 2)
     return image
 i = 0
+
 for img in glob.glob("Data_Extraction/Images/*.jpg"): #loading images from folder
     cv_img = cv2.imread(img)  #reading the image
     image = sharpen(cv_img) #sharpen the image
@@ -22,3 +26,6 @@ for img in glob.glob("Data_Extraction/Images/*.jpg"): #loading images from folde
     image = contour(gimage)
     cv2.imwrite(f"Data_Extraction/processed_images/{i}.jpg", image) #saving it 
     i = i + 1
+    
+    
+    
